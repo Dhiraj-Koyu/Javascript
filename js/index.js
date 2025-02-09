23f3287a69f9a0873cd92ff51d3fb56edd981d2e
Dhiraj-Koyu <Dhirajkoyu@gmail.com>
2025-02-09 18:01:51 +0545

    variables study

--- js/index.js
+++ js/index.js
@@ -1,10 +1,40 @@
-console.log("hello");
+console.log("hello world");
+var name = "Dhiraj"; //variable declare gareko const ley
+name = "Sujin"; //override
+console.log(name);
 
-const person = "Dhiraj";
+//* var, let, const
+//*ES5(Ecma Script 5)
+//*variable=>var
+
+//*ES6
+//*let,const
+//* const
+const person = "Figaland";
 console.log(person);
 
-let person1 = "slash";
-console.log(person1);
+//*let
+let school = "abc";
+school = "cdc";
+console.log(school);
 
-var person2 = "ozzy";
-console.log(person2);
+// alert("hello world");
+// const user = prompt("what is your name");
+// console.log(user);
+
+const isstudent = confirm("are you above 18");
+console.log(isstudent);
+
+//write a program that asks user for their name and alerts their name with greeting
+
+//*1
+const user = prompt("what is your name?");
+console.log(user);
+
+const greeter = "welcome back," + user;
+
+alert(greeter);
+
+//*2 string literal
+
+alert(`welcome back, ${user}`);
