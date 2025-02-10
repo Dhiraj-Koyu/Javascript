015ad6ee767cc80c4333f1039d18279d02d88297
Dhiraj-Koyu <Dhirajkoyu@gmail.com>
2025-02-10 18:00:22 +0545

    conditional def

---
+++ js/Statement.js
@@ -0,0 +1,39 @@
+// Conditional Operators
+
+//ES5
+// If-else
+//Switch-case
+
+//ES6
+//Ternary operator
+
+//* If-else
+// const isgender = prompt("Gender");
+// if (isgender === "m") {
+//   console.log("Male");
+// } else {
+//   console.log("female");
+// }
+
+//* Switch-case
+const day = +prompt("Enter day from 1 to 7");
+switch (day) {
+  case 1:
+    alert("sunday");
+    break;
+  case 2:
+    alert("moday");
+    break;
+  default:
+    alert("invalid number");
+}
+
+//Write a program that handles the grading
+//logic
+//80% and above distinction
+//70% - 79.99% :first division
+//60% - 69.99% :Second division
+//below 60%: Fail
+
+// jamesquick/javascript Array
+// asabehen/30 days
